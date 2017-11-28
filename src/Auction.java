1c00a1ea6ba976123751bd130f5b3b40409b5097
Rhodri23 <33624844+Rhodri23@users.noreply.github.com>
2017-11-28 15:49:54 +0000

    Update Auction.java

--- src/Auction.java
+++ src/Auction.java
@@ -1,11 +1,57 @@
-// Import statements
-
-/**
-* Simple description of the class, along with the author and verison.
-* @author Placeholder
-* @version 1.0
-*/
+import java.util.ArrayList; //Import statements
+import java.util.Date;
 
 class Auction {
-    // Class body
+
+	private ArrayList<Bid> bids = new ArrayList<Bid>(); //Holds the bids
+	
+	private Artwork art;
+	private User owner;
+	private Integer maxBids;
+	private float reserve;
+	private long timeCreated;
+	private boolean completed = false;
+
+	Date date = new Date(); //Creates new date object to use getTime(); method.
+
+	public Auction(Artwork art, Integer maxBids, float reserve, User owner) {
+
+		this.art = art;
+		this.maxBids = maxBids;
+		this.reserve = reserve;
+		this.owner = owner;
+		this.timeCreated = date.getTime(); //Sets the time in milliseconds when the auction was created.
+	}
+
+	public void setBid(Bid bid) {
+		bids.add(bid);
+	}
+
+	public Bid getBid(Integer i) {
+		return bids.get(i);
+	}
+
+	public float getReserve() {
+		return reserve;
+	}
+
+	public long getTime() {
+		return timeCreated;
+	}
+
+	public Artwork getArtwork() {
+		return art;
+	}
+
+	public User getUser() {
+		return owner;
+	}
+	
+	public Integer getMaxBids(){
+		return maxBids;
+	}
+
+	public boolean checkCompleted() {
+		return completed;
+	}
 }
